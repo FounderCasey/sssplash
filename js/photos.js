b78f5d583651dff916d7d241cee65cea823c2004
FounderCasey <caseywilcox@outlook.com>
2018-03-07 16:06:52 -0800

    Updated REST

--- js/photos.js
+++ js/photos.js
@@ -6,13 +6,14 @@ container.setAttribute('class', 'container');
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://api.unsplash.com/photos/curated/?client_id=47975a9a8b5b50e2ccd70194462bddbc5532e315b8fad6cc75e156eebd0a8b76&per_page=50', true)
+request.open('GET', 'https://api.unsplash.com/search/photos/?page=1&per_page=50&query=paris&client_id=47975a9a8b5b50e2ccd70194462bddbc5532e315b8fad6cc75e156eebd0a8b76', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
-  console.log(data);
+  var photos = data["results"];
+  console.log(photos);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(photo => {
+    photos.forEach(photo => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
